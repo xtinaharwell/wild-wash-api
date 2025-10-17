8164e6dacc071f6840dc57ea99c1d960e532c8a3
elijahcollins254 <elijahcollins254@gmail.com>
2025-10-17 10:58:59 +0000

    origins

--- api/settings.py
+++ api/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-firebase-wild-wash-apigit-1760697854679.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
+]
