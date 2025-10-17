9a3e906ca347daa3aa36ca235763c276d1a8e345
elijahcollins254 <elijahcollins254@gmail.com>
2025-10-17 10:56:28 +0000

    allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'wildwosh.kibeezy.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'wildwosh.kibeezy.com', 'https://8000-firebase-wild-wash-apigit-1760697854679.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev']
 
 
 # Application definition
